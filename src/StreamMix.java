a935a64263eff6ae2853c602a3c99ab209ed9cb5
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-04-30 12:38:16 +0300

    master

--- src/StreamMix.java
+++ src/StreamMix.java
@@ -3,16 +3,19 @@ import java.util.Iterator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class StreamMix<T>{
-    private int first;
-    private int second;
+public class StreamMix {
+//    private int first;
+//    private int second;
+
+    public StreamMix() {
+    }
 
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second)
     {
         Iterator<T> iteratorFirst = first.iterator();
         Iterator<T> iteratorSecond = second.iterator();
         Stream<T> mixStream = Stream.empty();
-        while (iteratorFirst.hasNext() && ((Iterator <?>)iteratorSecond).hasNext())
+        while (iteratorFirst.hasNext() && (iteratorSecond).hasNext())
         {
             mixStream = Stream.concat(mixStream, Stream.of(iteratorFirst.next(), iteratorSecond.next()));
         }
